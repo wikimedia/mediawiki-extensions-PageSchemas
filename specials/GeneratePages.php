434d6f5be97b3730603054679e7df8e8ec6820a9
Ankit Garg <ankitgarg833@users.mediawiki.org>
2011-06-13 14:16:47 +0000

    adding generateAllPages function and testing form submission

--- specials/GeneratePages.php
+++ specials/GeneratePages.php
@@ -6,22 +6,36 @@
  */
 
 class GeneratePages extends IncludableSpecialPage {
-        function __construct() {
-                parent::__construct( 'GeneratePages' );
-                wfLoadExtensionMessages('GeneratePages');
-        }
+    function __construct() {
+        parent::__construct( 'GeneratePages' );
+        wfLoadExtensionMessages('GeneratePages');
+    }
  
-        function execute( $par ) {
-                global $wgRequest, $wgOut;
- 
-                $this->setHeaders();
- 
-                # Get request data from, e.g.
-         $param = $wgRequest->getText('param');
- 
-                # Do stuff
-         # ...
-         $output="Hello world!".$param;
-                $wgOut->addWikiText( $output );
-        }
+    function execute( $category ) {
+		global $wgRequest, $wgOut;
+        $this->setHeaders();
+	
+		# Get request data from, e.g.
+        $param = $wgRequest->getText('param');
+		if ( $param != "" ) {
+			$this->generateAllPages($param);
+		
+		}
+		$text =<<< END
+<form method="post">        
+		<input type="hidden" name="param" value="$category" /><br />  
+		<input type="submit" value="Generate Pages" />
+</form>
+		
+END;
+		$wgOut->addHTML($text);
+				
+    }
+		
+	function generateAllPages ( $category_name ) {
+		global $wgRequest, $wgOut;
+	  $wgOut->addWikiText( $category_name );
+	
+	}
+		
 }
