ce62909181d44a09182b40f279df619d0faeb9ed
Ankit Garg <ankitgarg833@users.mediawiki.org>
2011-06-25 19:39:30 +0000

    adding support for error conditions

--- specials/GeneratePages.php
+++ specials/GeneratePages.php
@@ -14,23 +14,46 @@ class GeneratePages extends IncludableSpecialPage {
     function execute( $category ) {
 		global $wgRequest, $wgOut;
         $this->setHeaders();
-	
-		# Get request data from, e.g.
-        $param = $wgRequest->getText('param');
-		if ( $param != "" ) {
-			$this->generate_pages($param);
-		
-		}
 		$generate_page_text = wfMsg( 'ps-generate-pages' );
-		$text =<<< END
-<form method="post">        
-		<input type="hidden" name="param" value="$category" /><br />  
-		<input type="submit" value="$generate_page_text" />
-</form>
+		$generate_page_desc = wfMsg( 'ps-generate-pages-desc' );
+		$param = $wgRequest->getText('param');		
+		$text_1 = '<p>All pages will be generated! </p>';				
+		$text_2 = '<form method="post">  <input type="hidden" name="param" value="'.$category.'" /><br />  <input type="submit" value="'.$generate_page_text.'" /> </form>';
+		if ( $param != "" &&  $category != "" ) {		
+			$this->generate_pages($param);
+			$wgOut->addHTML($text_1);
+		}else {
+			if( $category == ""){
+				## Code to Display the list of categories
+			}else {
+			//this is when Special:GeneratePages/Category is accessed first time 
+			//Here check for the valid Category  name and allow for generating pages 
+				$title = Title::newFromText( $category, NS_CATEGORY );
+				$pageId = $title->getArticleID(); 		
+				$dbr = wfGetDB( DB_SLAVE );
+				//get the result set, query : slect page_props
+				$res = $dbr->select( 'page_props',
+				array(
+					'pp_page',
+					'pp_propname',
+					'pp_value'	
+				),
+				array(
+					'pp_page' => $pageId,
+					'pp_propname' => 'PageSchema'
+				)
+				);	
+				//first row of the result set 
+				$row = $dbr->fetchRow( $res );
+				if( $row != null ){
+					$wgOut->addHTML($text_2);
+				}else {
+						$text = "<p>No such category exist ! </p>";
+						$wgOut->addHTML( $text );
+				}				
+			}
 		
-END;
-		$wgOut->addHTML($text);
-				
+		}															
     }
 		
 	function generate_pages ( $categoryName ) {
@@ -39,6 +62,5 @@ END;
 		$pageSchema->generateAllPages();					
 	
 	
-	}
-		
+	}		
 }
