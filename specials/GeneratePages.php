6dcf0924d3cd284d5e799025e92801976f9c828b
Ankit Garg <ankitgarg833@users.mediawiki.org>
2011-07-23 18:21:11 +0000

    modifying code to use isPSDefined func. in PSSchema class.

--- specials/GeneratePages.php
+++ specials/GeneratePages.php
@@ -55,27 +55,13 @@ class GeneratePages extends IncludableSpecialPage {
 			}else {
 			//this is when Special:GeneratePages/Category is accessed first time 
 			//Here check for the valid Category  name and allow for generating pages 
-				$title = Title::newFromText( $category, NS_CATEGORY );
-				$pageId = $title->getArticleID();			
-				$dbr = wfGetDB( DB_SLAVE );
-				//get the result set, query : slect page_props
-				$res = $dbr->select( 'page_props',
-				array(
-					'pp_page',
-					'pp_propname',
-					'pp_value'	
-				),
-				array(
-					'pp_page' => $pageId,
-					'pp_propname' => 'PageSchema'
-				)
-				);
-				//first row of the result set 
-				$row = $dbr->fetchRow( $res );
-				if( $row != null ){
+				$pageSchemaObj = new PSSchema( $category );
+				if(!$pageSchemaObj->isPSDefined()){
+					$text = "<p>Error: there is no psssage schema defined for that category in the wiki. </p>";
+					$wgOut->addHTML( $text );
+				}else{								
 					$text_2 = '<p>'.$generate_page_desc.'</p> <form method="post">  <input type="hidden" name="param" value="'.$category.'" /><br />  ';
-					//add code to generate a list of check-box for pages to be generated.
-					$pageSchemaObj = new PSSchema( $category );
+					//add code to generate a list of check-box for pages to be generated.					
 					$pageList = array();
 					wfRunHooks( 'PageSchemasGetPageList', array( $pageSchemaObj, &$pageList ));	//will return an array of string, with each value as a title of the page to be created.					
 					foreach( $pageList as $page ){
@@ -85,11 +71,8 @@ class GeneratePages extends IncludableSpecialPage {
 						$text_2 .= '<input type="checkbox" name="page[]" value="'.$page_val.'" />  '.$page_link.' <br />';
 					}
 					$text_2 .= '<br /> <input type="submit" value="'.$generate_page_text.'" /> <br /> <br /></form>';
-					$wgOut->addHTML($text_2);
-				}else {
-						$text = "<p>Error: there is no page schema defined for that category in the wiki. </p>";
-						$wgOut->addHTML( $text );
-				}
+					$wgOut->addHTML($text_2);				
+			}
 			}
 		}
     }
