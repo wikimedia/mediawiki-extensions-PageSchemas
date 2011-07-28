da0c3d239717e8f2adce90a75fb0f874e1004e3f
Ankit Garg <ankitgarg833@users.mediawiki.org>
2011-07-28 16:51:05 +0000

    1_ Added code to create Category page, from EditSchema form. 2_ added jQuery to dynamically create delimiter input

--- specials/PS_EditSchema.php
+++ specials/PS_EditSchema.php
@@ -32,6 +32,7 @@ function createTemplateAddField(template_num) {
 			.fadeOut('fast', function() { jQuery(this).remove(); });
 	});
 	jQuery('#fieldsList_'+template_num).append(newField);
+	addjQueryToCheckbox();
 }
 function createAddTemplate() {
 	templateNum++;
@@ -45,7 +46,16 @@ function createAddTemplate() {
 	});
 	jQuery('#templatesList').append(newField);
 }
-
+function addjQueryToCheckbox(){
+	jQuery('.isListCheckbox').click(function() {		
+			if (jQuery(this).is(":checked"))
+			{				
+				jQuery(this).closest('.fieldBox').find('.delimiterInput').css('display', '');
+			}else{				
+				jQuery(this).closest('.fieldBox').find('.delimiterInput').css('display', 'none');
+			}
+	});
+}
 jQuery(document).ready(function() {
 	jQuery(".deleteField").click( function() {
 		// Remove the encompassing div for this instance.
@@ -57,17 +67,7 @@ jQuery(document).ready(function() {
 		jQuery(this).closest(".templateBox")
 			.fadeOut('fast', function() { jQuery(this).remove(); });
 	});
-	jQuery('.isListCheckbox').click(function() {
-			if (jQuery(this).is(":checked"))
-			{				
-				alert("checked");
-				jQuery(this).closest('.fieldBox').find('.delimiterInput').css('display', '');
-			}else{
-				alert("unchecked");
-				jQuery(this).siblings('.delimiterInput').css('display', 'none');
-			}  
-	});
-
+	addjQueryToCheckbox();
 });
 
 </script>
@@ -76,57 +76,12 @@ END;
 		$wgOut->addScript( $jsText );
 	}
    function execute( $category ) {
-		global $wgRequest, $wgOut;
+		global $wgRequest, $wgOut, $wgUser;
 		global $wgSkin;
         $this->setHeaders();		
 		$text_1 = '<p>This category does not exist yet. Create this category and its page schema: </p>';
 		$text_2 = '<p>This category exists, but does not have a page schema. Create schema:" </p>';
-		$save_page = $wgRequest->getCheck( 'wpSave' );		
-		if ($save_page) {
-			//Generate the Xml from the Form elements
-			$Xmltext  = "";
-			$s_name = $wgRequest->getText('s_name');
-			$Xmltext .= '<PageSchema name="'.$s_name.'">';
-			$ps_add_xml = $wgRequest->getText('ps_add_xml');
-			$Xmltext .= $ps_add_xml;			
-			foreach ( $wgRequest->getValues() as $var => $val ) {			
-				if(substr($var,0,7) == 't_name_'){
-					$Xmltext .= '<Template name="'.$val.'">';
-				}else if(substr($var,0,7) == 'f_name_'){
-					$Xmltext .= '<Field name="'.$val.'">';
-				}else if(substr($var,0,8) == 'f_label_'){
-					$Xmltext .= '<Label>'.$val.'</Label>';
-				}else if(substr($var,0,10) == 'f_add_xml_'){
-					$Xmltext .= $val;
-					$Xmltext .= '</Field>';
-				}else if(substr($var,0,10) == 't_add_xml_'){
-					$Xmltext .= $val;
-					$Xmltext .= '</Template>';
-				}
-			}
-			$Xmltext .= '</PageSchema>';			
-		}
-		else{
-		   if ( $category != "" ) {
-
-			$title = Title::newFromText( $category, NS_CATEGORY );
-			$pageId = $title->getArticleID();			
-			$dbr = wfGetDB( DB_SLAVE );
-			//get the result set, query : slect page_props
-			$res = $dbr->select( 'page_props',
-			array(
-				'pp_page',
-				'pp_propname',
-				'pp_value'	
-			),
-			array(
-				'pp_page' => $pageId,			
-				)
-			);
-			//first row of the result set 
-			$row = $dbr->fetchRow( $res );
-			if( $row == null ){
-			//Create form here
+		$text_3 = '<p>This category exists,have a page schema. Edit schema:" </p>';
 				self::addJavascript();
 				$text = "";
 				$text .= '<p>This category does not exist yet. Create this category and its page schema: </p>';
@@ -153,10 +108,10 @@ END;
 				<p>Additional XML:
 				<textarea rows=4 style="width: 100%" name="f_add_xml_1"></textarea> 
 				</p> 
-				<input type="button" value="Remove field" class="deleteField" /> </div>
-					
-</div>	
-			</fieldset> ';
+				<input type="button" value="Remove field" class="deleteField" /></fieldset>
+				</div>			
+			</div>	
+			';
 			$add_field_button = Xml::element( 'input',
 			array(
 				'type' => 'button',
@@ -204,25 +159,109 @@ END;
 		<hr /> ';
 		$text .= '<div class="fieldBox" id="starterField" style="display: none">
 				<fieldset style="background: #bbb;"><legend>Field</legend> 
+				<p>
 				<input size="15" name="f_name_starter">
 				Display label: <input size="15" name="f_label_starter">
 				</p>
-			<p><input type="checkbox" name="f_is_list_starter" /> This field can hold a list of values, separated by commas
-	&#160;&#160;
-	<p>Delimiter for values (default is ","): <input type="text" name="f_delimiter_starter" /> </p>
+			<p><input type="checkbox" name="f_is_list_starter" class="isListCheckbox" /> This field can hold a list of values
+	&#160;&#160;</p>
+	<div class="delimiterInput"  style="display: none" ><p>Delimiter for values (default is ","): <input type="text" name="f_delimiter_starter" /> </p></div>
 	<p>Additional XML:
 				<textarea rows=4 style="width: 100%" name="f_add_xml_starter"></textarea> 
 				</p> 
 				<input type="button" value="Remove field" class="deleteField" />
-</p>
 </fieldset>
 </div>';
+		$save_page = $wgRequest->getCheck( 'wpSave' );
+		if ($save_page) {
+			//Generate the Xml from the Form elements
+			$Xmltext  = "";
+			$s_name = $wgRequest->getText('s_name');
+			$Xmltext .= '<PageSchema name="'.$s_name.'">';
+			$ps_add_xml = $wgRequest->getText('ps_add_xml');
+			$Xmltext .= $ps_add_xml;			
+			$fieldName = "";
+			$fieldNum= -1;
+			$templateNum = -1;
+			foreach ( $wgRequest->getValues() as $var => $val ) {			
+				if(substr($var,0,7) == 't_name_'){
+					$templateNum = substr($var,7,1);					
+					if($wgRequest->getCheck( 'is_multiple_'.$templateNum )){
+						$Xmltext .= '<Template name="'.$val.'" multiple="multiple">';
+					}else{
+						$Xmltext .= '<Template name="'.$val.'">';
+					}
+				}else if(substr($var,0,7) == 'f_name_'){
+					$fieldName = $val;
+					$fieldNum = substr($var,7,1);
+					if($wgRequest->getCheck( 'f_is_list_'.$fieldNum )){
+						if( $wgRequest->getText('f_delimiter_'.$fieldNum) != ''){
+							$delimeter = $wgRequest->getText('f_delimiter_'.$fieldNum);
+							$Xmltext .= '<Field name="'.$fieldName.'" list="list" delimiter="'.$delimeter.'">';
+						}else{
+							$Xmltext .= '<Field name="'.$fieldName.'" list="list">';							
+						}
+					}else{
+						$Xmltext .= '<Field name="'.$fieldName.'">';
+					}
+				}else if(substr($var,0,8) == 'f_label_'){
+					$Xmltext .= '<Label>'.$val.'</Label>';
+				}else if(substr($var,0,10) == 'f_add_xml_'){
+					$Xmltext .= $val;
+					$Xmltext .= '</Field>';
+				}else if(substr($var,0,10) == 't_add_xml_'){
+					$Xmltext .= $val;
+					$Xmltext .= '</Template>';
+				}
+			}
+			$Xmltext .= '</PageSchema>';
+			
+			$categoryTitle = Title::newFromText( $category, NS_CATEGORY );
+			$categoryArticle = new Article( $categoryTitle );
+			$pageText = $categoryArticle->getContent();
+			$jobs = array();
+			if( $wgRequest->getText('is_edit')=='true' ){
+				//Do some preg-replace magic
+			}else{
+				$title = Title::newFromText( $category, NS_CATEGORY );
+				$params = array();
+				$params['user_id'] = $wgUser->getId();
+				$params['page_text'] = $pageText.$Xmltext;
+				$jobs[] = new PSCreatePageJob( $title, $params );	
+				Job::batchInsert( $jobs );
+			}			
+		}
+		else{
+		   if ( $category != "" ) {
+
+			$title = Title::newFromText( $category, NS_CATEGORY );
+			$pageId = $title->getArticleID();			
+			$dbr = wfGetDB( DB_SLAVE );
+			//get the result set, query : slect page_props
+			$res = $dbr->select( 'page_props',
+			array(
+				'pp_page',
+				'pp_propname',
+				'pp_value'	
+			),
+			array(
+				'pp_page' => $pageId,			
+				)
+			);
+			//first row of the result set 
+			$row = $dbr->fetchRow( $res );
+			if( $row == null ){
+				//Create form here, Cat doesnt exist, create new cat with this text
+				$text .= $text_1;
 				$wgOut->addHTML( $text );
 			}else{
 			  if( ($row[1] == 'PageSchema') && ($row[2] != null )){
-				
+				//Populate the form here with autocompleted values 
+				$pageXml = $row[2];
+				$wgOut->addHTML($text_3);
 			  }else{
-				$wgOut->addHTML($text_2);
+				$text .= $text_2;
+				$wgOut->addHTML($text);
 			  }
 			}
 		}else {
