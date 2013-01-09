568600e3527106a6fbb8456d14c2db6aa529dd9b
yaron <yaron57@gmail.com>
2013-01-10 00:32:58 +0300

    Added handling for new ContentHandler framework in MW 1.21
    Change-Id: I7793a6e951540610d2b327c8d7b6d21fd9f06fb1

--- PS_CreatePageJob.php
+++ PS_CreatePageJob.php
@@ -23,11 +23,21 @@ class PSCreatePageJob extends Job {
 			wfProfileOut( __METHOD__ );
 			return false;
 		}
-		$article = new Article( $this->title );
-		if ( !$article ) {
-			$this->error = 'pageSchemasCreatePage: Article not found "' . $this->title->getPrefixedDBkey() . '"';
-			wfProfileOut( __METHOD__ );
-			return false;
+		if ( method_exists( 'WikiPage', 'getContent' ) ) {
+			// MW >= 1.21
+			if ( $this->title->getContentModel() !== CONTENT_MODEL_WIKITEXT ) {
+				$this->error = 'pageSchemasCreatePage: Wiki page "' . $this->title->getPrefixedDBkey() . '" does not hold regular wikitext.';
+				wfProfileOut( __METHOD__ );
+				return false;
+			}
+			$wikiPage = new WikiPage( $this->title );
+		} else {
+			$article = new Article( $this->title );
+			if ( !$article ) {
+				$this->error = 'pageSchemasCreatePage: Article not found "' . $this->title->getPrefixedDBkey() . '"';
+				wfProfileOut( __METHOD__ );
+				return false;
+			}
 		}
 
 		$page_text = $this->params['page_text'];
@@ -38,7 +48,13 @@ class PSCreatePageJob extends Job {
 		$actual_user = $wgUser;
 		$wgUser = User::newFromId( $this->params['user_id'] );
 		$edit_summary = wfMsgForContent( 'ps-generatepages-editsummary' );
-		$article->doEdit( $page_text, $edit_summary );
+		if ( method_exists( 'WikiPage', 'getContent' ) ) {
+			// MW >= 1.21
+			$content = new WikitextContent( $page_text );
+			$wikiPage->doEditContent( $content, $edit_summary );
+		} else {
+			$article->doEdit( $page_text, $edit_summary );
+		}
 		$wgUser = $actual_user;
 		wfProfileOut( __METHOD__ );
 		return true;
