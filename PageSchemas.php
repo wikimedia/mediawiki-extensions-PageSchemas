904db4a081800e0a5a316edcdc22a67db03392c6
yaron <yaron57@gmail.com>
2014-09-10 20:52:19 +0400

    Removed 'jquery' as dependency - was never needed, causes problems for 1.24+
    Change-Id: I99cbaa6b781e3584a91ed71cdf307cbee2e71623

--- PageSchemas.php
+++ PageSchemas.php
@@ -86,7 +86,6 @@ $wgResourceModules += array(
 			'PageSchemas.css',
 		),
 		'dependencies' => array(
-			'jquery',
 		),
 	),
 );
@@ -98,7 +97,6 @@ $wgResourceModules += array(
 		'styles' => array(
 		),
 		'dependencies' => array(
-			'jquery',
 		),
 	),
 );
