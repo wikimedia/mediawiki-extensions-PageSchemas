e17752dee482b71b336319e69f31a4abc9b9f22e
zoranzoki21 <zorandori4444@gmail.com>
2020-04-09 19:46:53 +0200

    Move PSPageSection class into the own file
    Change-Id: I1af27a12c3b44be5cbfc05351d7b38314c9f0fed

---
+++ includes/PSPageSection.php
@@ -0,0 +1,34 @@
+<?php
+
+class PSPageSection {
+
+	private $mPageSectionXML = null;
+	private $mSectionName = "";
+	private $mSectionLevel = 2;
+
+	function __construct( $pageSectionXML ) {
+		$this->mPageSectionXML = $pageSectionXML;
+		$this->mSectionName = (string)$pageSectionXML->attributes()->name;
+		$this->mSectionLevel = (string)$pageSectionXML->attributes()->level;
+	}
+
+	public function getSectionName() {
+		return $this->mSectionName;
+	}
+
+	public function getSectionLevel() {
+		return $this->mSectionLevel;
+	}
+
+	public function getObject( $objectName ) {
+		global $wgPageSchemasHandlerClasses;
+
+		foreach ( $wgPageSchemasHandlerClasses as $psHandlerClass ) {
+			$object = call_user_func( array( $psHandlerClass, 'createPageSchemasObject' ), $objectName, $this->mPageSectionXML );
+			if ( !is_null( $object ) ) {
+				return $object;
+			}
+		}
+		return null;
+	}
+}
