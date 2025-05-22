f86026f77cc54b97ebbb27e689dddb610f5e047e
Fomafix <fomafix@googlemail.com>
2025-05-22 06:10:18 +0000

    Activate Phan
    Change-Id: Ib4145534e86a5ed39a59bc9e768309c8b2e32a71

---
+++ .phan/config.php
@@ -0,0 +1,23 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['suppress_issue_types'] = array_merge(
+	$cfg['suppress_issue_types'],
+	[
+		// Suppress issue types that currently exist in the codebase.
+		'MediaWikiNoEmptyIfDefined',
+		'PhanParamTooFewInPHPDoc',
+		'PhanTypeMismatchArgument',
+		'PhanTypeMismatchArgumentProbablyReal',
+		'PhanTypeMismatchDefault',
+		'PhanUndeclaredClassMethod',
+		'PhanUndeclaredClassReference',
+		'PhanUndeclaredMethod',
+		'PhanUndeclaredProperty',
+		'PhanUndeclaredVariable',
+		'SecurityCheck-DoubleEscaped'
+	]
+);
+
+return $cfg;
